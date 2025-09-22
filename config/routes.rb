5154752b5ce9573054ed53b0234816f37ea958e9
Georg Keferboeck <georg@keferboeck.com>
2025-09-23 01:46:51 +0200

    App Platform: add /up healthcheck and exclude from SSL redirect

--- config/routes.rb
+++ config/routes.rb
@@ -46,5 +46,6 @@ Rails.application.routes.draw do
     end
   end
 
+  get "/up", to: proc { [200, { "Content-Type" => "text/plain" }, ["OK"]] }
   get "/health", to: proc { [200, { "Content-Type" => "application/json" }, [{ ok: true }.to_json]] }
 end
