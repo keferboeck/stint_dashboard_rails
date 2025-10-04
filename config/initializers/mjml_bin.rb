e0813081f5047410bed23ed4643a8cd6a9956c34
Georg Keferboeck <georg@keferboeck.com>
2025-10-04 18:14:21 +0200

    progressing

--- config/initializers/mjml_bin.rb
+++ config/initializers/mjml_bin.rb
@@ -1,8 +1,17 @@
+# Use mjml-rails if present, but never crash the app if MJML is missing.
 if defined?(Mjml)
   Mjml.setup do |config|
-    node_bin = Rails.root.join("node_modules", ".bin", "mjml")
-    config.mjml_bin = node_bin.to_s if File.exist?(node_bin)
-    # Don't raise exceptions if mjml isn't present at boot
-    config.raise_render_exception = false if config.respond_to?(:raise_render_exception)
+    # Prefer explicit binary via ENV, otherwise try local node_modules
+    if ENV["MJML_BINARY"].present?
+      config.mjml_binary = ENV["MJML_BINARY"]
+    else
+      node_bin = Rails.root.join("node_modules", ".bin", "mjml")
+      config.mjml_binary = node_bin.to_s if File.exist?(node_bin)
+    end
+
+    # Important: don't raise on missing binary; log & continue
+    if config.respond_to?(:raise_render_exception)
+      config.raise_render_exception = false
+    end
   end
 end
