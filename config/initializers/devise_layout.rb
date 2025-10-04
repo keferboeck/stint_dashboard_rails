3dbd90a13fdb5c5922d1860220bacedbeec1ffc3
Georg Keferboeck <georg@keferboeck.com>
2025-10-05 00:32:22 +0200

    converted application from haml to erb

--- config/initializers/devise_layout.rb
+++
@@ -1,6 +0,0 @@
-# Make Devise use the main application layout, but only after Devise is loaded.
-Rails.application.config.to_prepare do
-  if defined?(Devise) && defined?(DeviseController)
-    DeviseController.layout "application"
-  end
-end
