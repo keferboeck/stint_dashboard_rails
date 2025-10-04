552de41ef238743e7f322f3af9e72dd2d32c096d
Georg Keferboeck <georg@keferboeck.com>
2025-10-05 00:23:52 +0200

    forcing application layout devise

--- config/initializers/devise_layout.rb
+++ config/initializers/devise_layout.rb
@@ -1,2 +1,6 @@
-# Make all Devise views use the main application layout
-DeviseController.layout "application"
+# Make Devise use the main application layout, but only after Devise is loaded.
+Rails.application.config.to_prepare do
+  if defined?(Devise) && defined?(DeviseController)
+    DeviseController.layout "application"
+  end
+end
