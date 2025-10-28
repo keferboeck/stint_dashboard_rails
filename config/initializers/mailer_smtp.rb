3b8bf9674e2ae4ad4d4ab201d892948f07ff6713
Georg Keferboeck <georg@keferboeck.com>
2025-10-28 22:19:26 +0100

    fix localhost issue on staging

--- config/initializers/mailer_smtp.rb
+++ config/initializers/mailer_smtp.rb
@@ -1,27 +1,17 @@
 # config/initializers/mailer_smtp.rb
 Rails.application.configure do
-  if ENV["MANDRILL_API_KEY"].present?
-    config.action_mailer.perform_caching       = false
-    config.action_mailer.perform_deliveries    = true
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.delivery_method       = :smtp
-    config.action_mailer.smtp_settings = {
-      address:              "smtp.mandrillapp.com",
-      port:                 587,
-      user_name:            "mandrill", # any string or your Mandrill login email works
-      password:             ENV["MANDRILL_API_KEY"],
-      authentication:       :login,
-      enable_starttls_auto: true
-    }
+  next unless ENV["MANDRILL_API_KEY"].present?
 
-    # sensible defaults for all mailers
-    default_host = ENV.fetch("APP_HOST", "localhost")
-    default_proto = Rails.env.development? ? "http" : "https"
-
-    config.action_mailer.default_url_options = {
-      host:     default_host,
-      protocol: default_proto
-    }
-    Rails.application.routes.default_url_options = config.action_mailer.default_url_options
-  end
+  config.action_mailer.perform_caching       = false
+  config.action_mailer.perform_deliveries    = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method       = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.mandrillapp.com",
+    port:                 587,
+    user_name:            "mandrill",
+    password:             ENV["MANDRILL_API_KEY"],
+    authentication:       :login,
+    enable_starttls_auto: true
+  }
 end
