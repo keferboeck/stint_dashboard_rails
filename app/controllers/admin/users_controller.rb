a695ff4d7fcd9653b193d6004876c8a4e7e920ab
Georg Keferboeck <georg@keferboeck.com>
2025-10-28 14:57:24 +0100

    editing users works

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -10,11 +10,22 @@ class Admin::UsersController < ApplicationController
 
   def new
     @user = User.new(role: "viewer")
+    # Optional server side defaults for a brand new staff user
+    # if params.dig(:user, :role) == "staff" || @user.role == "staff"
+    #   @user.assign_attributes(
+    #     notify_new_scheduled_mine: true,
+    #     notify_copy_mine:          true,
+    #     notify_summary_mine:       true
+    #   )
+    # end
   end
 
   def create
     @user = User.new(user_params)
-    # Generate a temporary password (device email can be added later)
+
+    # Enforce permissions after mass assignment
+    enforce_notification_permissions(@user)
+
     temp_password = SecureRandom.base58(16)
     @user.password = @user.password_confirmation = temp_password
 
@@ -30,7 +41,11 @@ class Admin::UsersController < ApplicationController
   def edit; end
 
   def update
-    if @user.update(user_params)
+    # Assign first, then enforce permissions, then save
+    @user.assign_attributes(user_params)
+    enforce_notification_permissions(@user)
+
+    if @user.save
       flash[:notice] = "User updated."
       redirect_to admin_users_path
     else
@@ -50,15 +65,34 @@ class Admin::UsersController < ApplicationController
     redirect_to dashboard_path, alert: "Not authorized."
   end
 
+  # Always permit all flags. We will zero out disallowed ones based on the final role.
   def user_params
     params.require(:user).permit(
-      :first_name, :last_name, :email, :role,
-      :notify_all_new_schedules,
-      :notify_all_sent_copies,
-      :notify_all_campaign_summaries,
-      :notify_own_new_schedules,
-      :notify_own_sent_copies,
-      :notify_own_campaign_summaries
+      :first_name, :last_name, :email, :role, :preferred_time_zone,
+      :notify_new_scheduled_all, :notify_copy_all, :notify_summary_all,
+      :notify_new_scheduled_mine, :notify_copy_mine, :notify_summary_mine
     )
+  end
+
+  # Normalize flags according to the effective role (param wins over current)
+  def enforce_notification_permissions(user)
+    effective_role = user.role.presence || "viewer"
+
+    case effective_role
+    when "admin"
+      # allowed: all six flags, do nothing
+    when "staff"
+      user.notify_new_scheduled_all = false
+      user.notify_copy_all          = false
+      user.notify_summary_all       = false
+      # mine flags allowed as submitted
+    else # viewer
+      user.notify_new_scheduled_all = false
+      user.notify_copy_all          = false
+      user.notify_summary_all       = false
+      user.notify_new_scheduled_mine = false
+      user.notify_copy_mine          = false
+      user.notify_summary_mine       = false
+    end
   end
 end
